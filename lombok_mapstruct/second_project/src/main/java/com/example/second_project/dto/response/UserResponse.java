0c4336f4a548542a367e8f505beadca8af1738b1
QuavyTwo <lequangvinh.127.2022@gmail.com>
2024-08-18 15:55:34 +0700

    lombok and mapstruct

---
+++ lombok_mapstruct/second_project/src/main/java/com/example/second_project/dto/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.example.second_project.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserResponse {
+    String id;
+    String username;
+    String password;
+    String firstname;
+    String lastname;
+    LocalDate dob;
+}
