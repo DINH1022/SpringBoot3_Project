45194d30f3667f9774f5258f780362f336835006
QuavyTwo <lequangvinh.127.2022@gmail.com>
2024-08-18 15:47:26 +0700

    Stage 1

---
+++ second_project/second_project/src/main/java/com/example/second_project/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.second_project.controller;
+
+import com.example.second_project.dto.request.UserCreationRequest;
+import com.example.second_project.dto.request.UserUpdateRequest;
+import com.example.second_project.entity.User;
+import com.example.second_project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping()
+    User createUser(@RequestBody UserCreationRequest request){
+        return userService.creationRequest(request);
+    }
+
+    @GetMapping()
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId,request );
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted.";
+    }
+}
